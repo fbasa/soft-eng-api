9f712de3422e719a08f7ebaede92d854003e11e5
Franklin Basa <basa.franklin@gmail.com>
2025-10-02 21:48:12 +0800

    Added CreatedDate & ModifiedDate

--- SoftEng/SoftEng.Domain/Response/GetStudentDetailsResponse.cs
+++ SoftEng/SoftEng.Domain/Response/GetStudentDetailsResponse.cs
@@ -2,5 +2,4 @@
 
 public class GetStudentDetailsResponse : GetStudentListResponse
 {
-
 }
--- SoftEng/SoftEng.Domain/Response/GetStudentListResponse.cs
+++ SoftEng/SoftEng.Domain/Response/GetStudentListResponse.cs
@@ -5,4 +5,7 @@ namespace SoftEng.Domain.Response;
 public class GetStudentListResponse : CreateStudentRequest
 {
     public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+
 }
