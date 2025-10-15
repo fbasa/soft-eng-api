b21e9154f9582b22caa0177a89449190e510f103
Kinley <kinleyjhonlavina01@gmail.com>
2025-10-15 15:46:45 +0800

    added HTTPGET schools, programs, and semesters

--- SoftEng/SoftEng.Api/Controllers/SharedController.cs
+++ SoftEng/SoftEng.Api/Controllers/SharedController.cs
@@ -15,4 +15,19 @@ public class SharedController() : ControllerBase
     {
         return Ok(new List<string> { "Male", "Female" });
     }
+    [HttpGet("Schools")]
+    public async Task<IActionResult> GetSchoolAsync(CancellationToken ct)
+    {
+        return Ok(new List<string> { "High School", "College", "University", "Other" });
+    }
+    [HttpGet("Programs")]
+    public async Task<IActionResult> GetProgramAsync(CancellationToken ct)
+    {
+        return Ok(new List<string> { "Computer Science", "Engineering", "Business", "Arts & Humanities", "Natural Sciences" });
+    }
+    [HttpGet("Semesters")]
+    public async Task<IActionResult> GetSemesterAsync(CancellationToken ct)
+    {
+        return Ok(new List<string> { "1st", "2nd" });
+    }
 }
